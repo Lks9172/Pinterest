361724fdf58be9814191c5d86921cb2e0d319807
Lks9172 <ekdan9172@naver.com>
2021-01-10 14:54:13 +0900

    27,28 인증시스템 구축과 decorator사용하여 코드 간소화

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,19 +1,26 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from accountapp.decorators import account_ownership_required
 from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 
+has_ownership = [account_ownership_required, login_required]
+
+
+# 데코레이터로 user 인증 됬을때만 hello_world에 접근할 수 있도록 해준다.
+@login_required
 def hello_world(request):
+
     if request.method == "POST":
-
         temp = request.POST.get('hello_world_input')
-
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
@@ -36,6 +43,8 @@ class AccountDetailView(DetailView):
     template_name = 'accountapp/detail.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountUpdateView(UpdateView):
     model = User
     context_object_name = 'target_user'
@@ -44,6 +53,8 @@ class AccountUpdateView(UpdateView):
     template_name = 'accountapp/update.html'
 
 
+@method_decorator(has_ownership, 'get')
+@method_decorator(has_ownership, 'post')
 class AccountDeleteView(DeleteView):
     model = User
     context_object_name = 'target_user'
